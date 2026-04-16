eccd7ea9a066bb21251c04f6336e9fe1f47bcf28
Kruthik B T <kruthikchinnu641@gmail.com>
2026-04-16 22:28:17 +0530

    Add files via upload

---
+++ Day_of_Arrays/01_problem.c
@@ -0,0 +1,16 @@
+// Write a program to print the price of 3 products with gst.
+
+#include <stdio.h>
+int main() {
+    float price[3];
+        printf("Enter the price of 3 items: ");
+        scanf("%f",&price[0]);
+        scanf("%f",&price[1]);
+        scanf("%f",&price[2]);
+
+        printf("Total price of First Item is :%f\n",price[0] + price[0]*0.18);
+        printf("Total price of second Item is :%f\n",price[1] + price[1]*0.18);
+        printf("Total price of Third Item is :%f\n",price[2] + price[2]*0.18);
+
+        return 0;
+}
