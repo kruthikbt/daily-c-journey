07a7d7becf1253b4faf56d61798d5e5fb81a79c2
Kruthik B T <kruthikchinnu641@gmail.com>
2026-04-18 22:44:23 +0530

    Add files via upload

---
+++ Day_of_Arrays/06_problem.c
@@ -0,0 +1,20 @@
+/* Write a program to print the fibonacci series by using Arrays*/
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the Number of Terms n (n>1): ");
+    scanf("%d",&n);
+
+    int fib[n];
+    fib[0]=0;
+    fib[1]=1;
+
+    for(int i=2;i<n;i++){
+        fib[i]=fib[i-1] + fib[i-2];
+        printf("%d",fib[i]);
+    
+    }
+    printf("\n");
+    return 0;
+}
