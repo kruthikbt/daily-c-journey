8d4d4405f691715a1ce32720fb2ab1029cd802e6
Kruthik B T <kruthikchinnu641@gmail.com>
2026-04-18 21:38:38 +0530

    Add files via upload

---
+++ Day_of_Arrays/03_problem.c
@@ -0,0 +1,19 @@
+/* Write a Function which uses array as the parameter ,and prints the numbers present in thr array*/
+
+#include <stdio.h>
+
+    void printNumbers(int arr[],int n);
+
+    int main() {
+
+    int arr[]={12,24,35,56};
+    printNumbers(arr,4);
+        return 0;
+    }
+
+void printNumbers(int arr[],int n){
+for(int i=0;i<n;i++){
+    printf(" %d\t",arr[i]);
+}
+}
+
