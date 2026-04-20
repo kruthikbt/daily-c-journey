5a998eef0f7e07d9d2818da647e3c9944a07f38e
Kruthik B T <kruthikchinnu641@gmail.com>
2026-04-20 23:50:33 +0530

    Add files via upload

---
+++ Day_of_Arrays/08_problem.c
@@ -0,0 +1,21 @@
+// Write a program to find the largest element
+
+#include <stdio.h>
+int main() {
+    int a[5], i, max;
+
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++)
+        scanf("%d", &a[i]);
+
+    max = a[0];
+
+    for(i = 1; i < 5; i++) {
+        if(a[i] > max)
+            max = a[i];
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
