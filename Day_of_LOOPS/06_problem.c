97862c75181e0c36aa2e752c08b880618d01549a
Kruthik B T <kruthikchinnu641@gmail.com>
2026-04-28 23:23:51 +0530

    Update 06_problem.c

--- Day_of_LOOPS/06_problem.c
+++ Day_of_LOOPS/06_problem.c
@@ -1,7 +1,7 @@
 /*Write a program to print whether the number entered by the user is possitive or negative by using ternary operator. */
 
 #include <stdio.h>
-int main() {
+int  main() {
 printf("Enter  the value of n:") ;
 scanf("%d", &n) ;
 
