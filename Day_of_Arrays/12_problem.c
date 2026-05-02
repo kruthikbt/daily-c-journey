58254769c885b4647caf30c900961a96b09f8849
Kruthik B T <kruthikchinnu641@gmail.com>
2026-05-02 21:36:07 +0530

    Create 12_problem.c

---
+++ Day_of_Arrays/12_problem.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {1, 3, 5};
+    int b[] = {2, 4, 6};
+    int c[6];
+
+    int i = 0, j = 0, k = 0;
+
+    while(i < 3 && j < 3) {
+        if(a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+
+    while(i < 3)
+        c[k++] = a[i++];
+
+    while(j < 3)
+        c[k++] = b[j++];
+
+    for(int x = 0; x < 6; x++)
+        printf("%d ", c[x]);
+
+    return 0;
+}
