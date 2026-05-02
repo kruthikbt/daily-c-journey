13001ef80db5871680b154cfe15d089042e229be
Kruthik B T <kruthikchinnu641@gmail.com>
2026-05-02 21:32:47 +0530

    Create 10_problem.c

---
+++ Day_of_Arrays/10_problem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 45, 23, 67, 67, 34};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int largest = -1, second = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if(arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second largest = %d", second);
+    return 0;
+}
