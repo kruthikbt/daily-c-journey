b6a1d57d79e4fbc7b022b0081bf280314e713428
Kruthik B T <kruthikchinnu641@gmail.com>
2026-05-16 23:16:04 +0530

    Add files via upload

---
+++ Day_of_Strings/5.c
@@ -0,0 +1,12 @@
+// Write a Program that inputs users ame and prints the length of the strng
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    printf("Entered the Desired String:");
+    scanf("%s",str);
+    printf("The Length of the string enteed is:%d",strlen(str));                                          
+}
