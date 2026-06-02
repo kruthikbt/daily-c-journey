0253997b06162d9cc83f79ce9a00518c29e01010
Kruthik B T <kruthikchinnu641@gmail.com>
2026-06-02 23:02:56 +0530

    Fix formatting and clean up comments in 01_problem.c

--- Day_of_Arrays/01_problem.c
+++ Day_of_Arrays/01_problem.c
@@ -1,6 +1,6 @@
 // Write a program to print the price of 3 products with gst.
 
-#include  <stdio.h>
+#include <stdio.h>
 int main()  {  
     float  price[3];
         printf("Enter the price of 3 items: ");
